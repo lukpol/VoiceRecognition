2db26e5545e2e74d2846864680c3263f9b2f4bac
Łukasz Pollak <lukaszpollak@macbook-pro-ukasz-2.home>
2020-01-11 12:26:40 +0100

    Initial commit

---
+++ parameterization.py
@@ -0,0 +1,27 @@
+def hamming_window(x):
+    y = x
+    return y
+
+
+def preemphasis(x):
+    y = x
+    return y
+
+
+def fft(x):
+    y = x
+    return y
+
+
+def dct(x):
+    y = x
+    return y
+
+
+def MFCC_filtering(x):
+    y = x
+    return y
+
+def parametrization(x):
+    x_windowed = hamming_window(x)
+    spectrum = fft()
